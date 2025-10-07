b21d0e97c7f4a0c5d991df3f54336740a3fba848
Maryssa RENE <maryssarene@MacBook-Air-de-Maryssa-2.local>
2025-10-07 14:12:14 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Site TomTroc</title>
+</head>
+
+<body>
+    <div class="v24_892">
+        <div class="v10_1008">
+            <div class="v10_1009"></div>
+            <div class="v10_1010"></div>
+            <div class="v10_1011"></div>
+        </div>
+        <div class="v10_1012"></div>
+        <div class="name"></div><span class="v10_1014">Rejoignez nos lecteurs passionnés </span>
+        <img src="./images/hero-tomtroc-books2.png" alt="Livres à échanger" class="v10_1015">
+        <div class="v10_1015"></div>
+        <span class="v10_1016">Donnez une nouvelle vie à vos livres en les échangeant avec
+            d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à
+            travers les livres. </span>
+        <span class="v10_1017">Échanger des livres avec TomTroc c’est simple et amusant !
+            Suivez ces étapes pour commencer :</span>
+        <span class="v10_1018">Chez Tom Troc, nous mettons l'accent sur le
+            partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et
+            notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour
+            rassembler les gens et inspirer des conversations enrichissantes.
+
+            Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.
+
+            Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter,
+            de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les
+            étagères.</span>
+        <div class="v10_1019">
+            <div class="v10_1020"></div><span class="v10_1021">Inscrivez-vous gratuitement sur
+                notre plateforme.</span>
+        </div>
+        <div class="v10_1022">
+            <div class="v10_1023"></div><span class="v10_1024">Proposez un échange et discutez avec d'autres passionnés
+                de lecture.</span>
+        </div>
+        <div class="v10_1025">
+            <div class="v10_1026"></div><span class="v10_1027">Parcourez les livres disponibles chez d'autres
+                membres.</span>
+        </div>
+        <div class="v10_1028">
+            <div class="v10_1029"></div><span class="v10_1030">Ajoutez les livres que vous souhaitez échanger à votre
+                profil.</span>
+        </div>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="name"></div><span class="v10_1035">Hamza </span><span class="v10_1036">Les derniers livres
+            ajoutés</span><span class="v10_1037">Comment ça marche ?</span><span class="v10_1038">Nos valeurs</span>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="v10_1044"></div><span class="v10_1045">L’équipe Tom Troc</span>
+    </div>
+</body>
+
+</html>
